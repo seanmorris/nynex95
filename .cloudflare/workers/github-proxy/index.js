12f73b791300bb8d197d6c603be795545546cdb7
Sean Morris <sean@seanmorr.is>
2020-08-15 18:59:34 -0400

    Correcting "Publish" action config.

--- .cloudflare/workers/github-proxy/index.js
+++ .cloudflare/workers/github-proxy/index.js
@@ -23,44 +23,42 @@ async function handleRequest(request) {
 
 		}).then(({response, responseText}) => {
 
-			const { readable, writable } = new TransformStream();
-
-			const writer = writable.getWriter();
-			const encoder = new TextEncoder();
-
 			const headers = new Headers(response.headers);
+			const rawBody = responseText.replace(/api\.github\.com/g, original.host);
 
-			headers.append('Content-Type', 'text/event-stream');
-			headers.append('Cache-Control', 'no-cache');
-			headers.append('Connection', 'keep-alive');
-			headers.append('Access-Control-Allow-Origin', '*');
-			headers.append('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-
-			const raw  = responseText.replace(/api\.github\.com/g, original.host);
-			const body = JSON.parse(raw);
-
-			if(Array.isArray(body))
+			if(origina.searchParams.get('api') == 'json-source')
 			{
-				for(const line of body)
+				headers.append('Content-Type', 'text/event-stream');
+				headers.append('Cache-Control', 'no-cache');
+				headers.append('Connection', 'keep-alive');
+				headers.append('Access-Control-Allow-Origin', '*');
+				headers.append('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+
+				const { readable, writable } = new TransformStream();
+				const writer = writable.getWriter();
+				const encoder = new TextEncoder();
+
+				const parsedBody = JSON.parse(rawBody);
+
+				if(Array.isArray(parsedBody))
 				{
-					writer.write(encoder.encode('data: ' + JSON.stringify(line) + '\n\n'));
+					for(const line of parsedBody)
+					{
+						writer.write(encoder.encode('data: ' + JSON.stringify(line) + '\n\n'));
+					}
 				}
+				else
+				{
+					writer.write(encoder.encode('data: ' + rawBody + '\n\n'));
+				}
+
+				return new Response(readable, {
+					'status':       200
+					, 'statusText': 'ok'
+					, 'headers':     headers
+				});
 			}
-			else
-			{
-				writer.write(encoder.encode('data: ' + raw + '\n\n'));
-			}
 
-			return new Response(readable, {
-				'status':       200
-				, 'statusText': 'ok'
-				, 'headers':     headers
-			});
-
-			// return new Response(
-			//
-			// 	, {headers:response.headers}
-			// );
-
+			return new Response(rawBody, {headers:response.headers});
 		});
 }
