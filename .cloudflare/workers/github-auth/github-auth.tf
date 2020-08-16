4e7f9f2c00eb39ca1e6b66afc35469b28080e84b
Sean Morris <sean@seanmorr.is>
2020-08-16 12:01:45 -0400

    Worker total refresh action

--- .cloudflare/workers/github-auth/github-auth.tf
+++ .cloudflare/workers/github-auth/github-auth.tf
@@ -1,9 +1,9 @@
 variable "CLOUDFLARE_ACCOUNT_ID" {
-  description = "Your Cloudflare account ID - https://developers.cloudflare.com/workers/quickstart#account-id-and-zone-id"
+	description = "Your Cloudflare account ID - https://developers.cloudflare.com/workers/quickstart#account-id-and-zone-id"
 }
 
 variable "CLOUDFLARE_API_TOKEN" {
-  description = "Your Cloudflare API Token - https://dash.cloudflare.com/profile/api-tokens"
+	description = "Your Cloudflare API Token - https://dash.cloudflare.com/profile/api-tokens"
 }
 
 provider "cloudflare" {
@@ -11,16 +11,16 @@ provider "cloudflare" {
 	api_token  = var.CLOUDFLARE_API_TOKEN
 }
 
-resource "cloudflare_workers_kv_namespace" "github-proxy-kv" {
-  title = "github-proxy-kv"
+resource "cloudflare_workers_kv_namespace" "github-auth-kv" {
+	title = "github-auth-kv"
 }
 
 resource "cloudflare_worker_script" "default" {
-  name    = "github-proxy"
-  content = file("index.js")
+	name    = "github-auth"
+	content = file("index.js")
 
-  kv_namespace_binding {
-    namespace_id = cloudflare_workers_kv_namespace.github-proxy-kv.id
-    name         = "PROXY_KV"
-  }
+	kv_namespace_binding {
+		namespace_id = cloudflare_workers_kv_namespace.github-auth-kv.id
+		name         = "PROXY_KV"
+	}
 }
