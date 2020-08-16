830e048d8baeb9fe08a1bc4ff019a6523d4cb4b3
Sean Morris <sean@seanmorr.is>
2020-08-16 09:20:27 -0400

    Worker total refresh action

--- .cloudflare/workers/github-proxy/spinup.sh
+++ .cloudflare/workers/github-proxy/spinup.sh
@@ -5,9 +5,7 @@ SCRIPT_NAME=github-proxy
 curl -X DELETE "https://api.cloudflare.com/client/v4/accounts/$TF_VAR_CLOUDFLARE_ACCOUNT_ID/workers/scripts/$SCRIPT_NAME" \
 	-H "Authorization: Bearer $TF_VAR_CLOUDFLARE_API_TOKEN"
 
-EXISTING_KV=`wrangler kv:namespace list | jq -r '.[] | select(.title == "github-proxy-kv").id'`;
-
-echo $EXISTING_KV;
+yes | wrangler kv:namespace delete --namespace-id=`wrangler kv:namespace list | jq -r '.[] | select(.title == "github-proxy-kv").id'`;
 
 # terraform init .;
 yes | wrangler kv:namespace delete --namespace-id=$EXISTING_KV;
@@ -15,6 +13,4 @@ yes | wrangler kv:namespace delete --namespace-id=$EXISTING_KV;
 
 terraform 0.13upgrade --yes .;
 
-terraform refresh .
-terraform plan .
-terraform apply --auto-approve .
+terraform init . && terraform plan . && terraform apply --auto-approve .
