60fdecc0fec1e97a04d3641c362e88d4e4361c09
Sean Morris <sean@seanmorr.is>
2020-08-17 01:09:22 -0400

    NamespaceID check

--- .cloudflare/workers/github-auth/index.js
+++ .cloudflare/workers/github-auth/index.js
@@ -7,26 +7,30 @@ addEventListener('fetch', event => {
 async function handleRequest(request) {
 
 	const sessionKey = crypto.getRandomValues(new Uint8Array(256)).join('');
+	const githubUrl  = 'https://github.com/login/oauth/access_token';
 
-	const githubUrl = 'https://github.com/login/oauth/access_token';
+	const url = new URL(request);
+	const GET = url.searchParams;
 
 	const headers = new Headers();
 	const method  = 'POST';
 	const body    = JSON.stringify({
-		client_id:     '7150d20fb5a11fe1d332'
-		, client_secret: ''
-		, code:          ''
-		, redirect_uri:  ''
-		, state:         ''
+		client_id:       GHAPI_CLIENT_ID
+		, client_secret: GHAPI_CLIENT_SECRET
+		, code:          GET.get('code')
+		, redirect_uri:  'https://github-auth.unholyshit.workers.dev/'
+		, state:         '---'
 	});
 
 	// AUTH_KV;
 
-	return fetch(githubUrl, {method, headers, body}).then(r => r.text()).then(r => {
+	return fetch(githubUrl, {method, headers, body}).then(r => r.text()).then(apiResponse => {
 
-		console.log(r);
+		console.log(apiResponse);
 
-		return new Response('ok.');
+		return new Response(apiResponse, {
+			headers: new Headers({'content-type': 'plaintext'})
+		});
 
 	});
 
