9bee04b4ddacffc27a4be904dd1afc0e9f82c62f
Sean Morris <sean@seanmorr.is>
2020-08-16 23:55:41 -0400

    Github API details in cloudflare vars.

--- .cloudflare/workers/github-proxy/github-proxy.tf
+++ .cloudflare/workers/github-proxy/github-proxy.tf
@@ -1,26 +1,34 @@
+variable "GHAPI_CLIENT_ID" {
+	description = "GitHub API client ID for your app."
+}
+
+variable "GHAPI_CLIENT_SECRET" {
+	description = "GitHub API client SECRET for your app."
+}
+
 variable "CLOUDFLARE_ACCOUNT_ID" {
-  description = "Your Cloudflare account ID - https://developers.cloudflare.com/workers/quickstart#account-id-and-zone-id"
+	description = "Your Cloudflare account ID - https://developers.cloudflare.com/workers/quickstart#account-id-and-zone-id"
 }
 
 variable "CLOUDFLARE_API_TOKEN" {
-  description = "Your Cloudflare API Token - https://dash.cloudflare.com/profile/api-tokens"
+	description = "Your Cloudflare API Token - https://dash.cloudflare.com/profile/api-tokens"
 }
 
 provider "cloudflare" {
-  account_id = var.CLOUDFLARE_ACCOUNT_ID
-  api_token  = var.CLOUDFLARE_API_TOKEN
+	account_id = var.CLOUDFLARE_ACCOUNT_ID
+	api_token  = var.CLOUDFLARE_API_TOKEN
 }
 
 resource "cloudflare_workers_kv_namespace" "github-proxy-kv" {
-  title = "github-proxy-kv"
+	title = "github-proxy-kv"
 }
 
 resource "cloudflare_worker_script" "default" {
-  name    = "github-proxy"
-  content = file("index.js")
+	name    = "github-proxy"
+	content = file("index.js")
 
-  kv_namespace_binding {
-    namespace_id = cloudflare_workers_kv_namespace.github-proxy-kv.id
-    name         = "PROXY_KV"
-  }
+	kv_namespace_binding {
+		namespace_id = cloudflare_workers_kv_namespace.github-proxy-kv.id
+		name         = "PROXY_KV"
+	}
 }
